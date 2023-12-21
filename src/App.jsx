a9934e14b88b056942aa9c904b970945d94fffc1
Astrick <mypiarenoo@gmail.com>
2023-12-22 05:56:06 +0700

    (Fixed): Button state on website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  const setHelloTH = () => {
+    setGreetingMessage("ขอตังหน่อย!");
+  };
+  const setHelloEN = () => {
+    setGreetingMessage("Hey!");
+  };
+  const setHelloCN = () => {
+    setGreetingMessage("Haiyaaaa!");
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={setHelloTH}>สวัสดี!</button>
+        <button onClick={setHelloEN}>Hi!</button>
+        <button onClick={setHelloCN}>你好!</button>
       </div>
     </div>
   );
